e19e3bc58f86c227650e330faa46e07808bdde24
Tim Darrow <timothy.honor7@gmail.com>
2018-12-12 15:06:13 -0800

    Changes to main program and added smaller test csv
    Removed lost and found url csvs. For now, changed save location of csvs to Downloads folders.
    Also built in logic to incorporate catch-all parameters

--- RedirectMachine/Program.cs
+++ RedirectMachine/Program.cs
@@ -8,20 +8,85 @@ namespace RedirectMachine
 {
     class Program
     {
-        // lost list 
+        // lost and found lists
         static List<string> lostList = new List<string>();
-        // found list 
-        static List<string> foundList = new List<string>();
-        // old list
+        public static List<string> foundList = new List<string>();
+
+        // site url lists
         static List<string> osUrls = new List<string>();
-        // new list
         static List<string> nsUrls = new List<string>();
 
         public static List<string> osBlogList = new List<string>();
         public static List<string> osDoctorList = new List<string>();
-        public static string[] osBlogParams = { "/news/", "/blogs/" };
-        public static string[] osDoctorParams = { "/provider/", "/providers/" };
-        public static string[,] osParams = { { "/bakersfield/pages/ehealth/kramescontent/", "/bakersfield/*" }, { "/castle/event/", "/castle/classes-and-events/*" }, { "/central-valley/ages/ehealth/kramescontent/", "/blog/*" } };
+        //public static List<string[]> priorityList = new List<string[]>();
+        public static string[,] priorityList = new string[,] {
+            {"", "" }
+        };
+
+
+        public static string[,] osParams =  { 
+            { "/bakersfield/pages/ehealth/kramescontent/", "/bakersfield/" }, 
+            { "/castle/event/", "/castle/classes-and-events/" },
+            { "/castle/pages/ehealth/kramescontent/", "/castle/" },
+            { "/castle/pages/ehealth/adamcontent/", "/castle/" },
+            { "/castle/pages/oham/orgunitdetails.aspx", "/castle/" },
+            { "/castle/pages/pnrs/", "/doctors/" },
+            { "/castle/pages/search/searchresults.aspx", "/castle/" },
+            { "/central-valley/pages/ehealth/kramescontent/", "/blog/" },
+            { "/central-valley/pages/pnrs/", "/doctors/" },
+            { "/clear-lake/pages/ehealth/kramescontent/", "/clear-lake/" },
+            { "/feather-river/pages/ehealth/kramescontent/", "/feather-river/" },
+            { "/glendale/Pages/eHealth/KramesContent/", "/glendale/" },
+            { "/glendale/pages/pnrs/providerprofile.aspx/", "/doctors/" },
+            { "/howard-memorial/Pages/ehealth/kramescontent/", "/howard-memorial/" },
+            { "/howard-memorial/pages/news/newssearchresult.aspx", "/blog/" },
+            { "/lodimemorial/Pages/ehealth/kramescontent/default.aspx/", "/lodi-memorial/" },
+            { "/lodimemorial/pages/enrs/eventscartregistration.aspx", "/lodi-memorial/" },
+            { "/lodimemorial/pages/enrs/eventssearchresult.aspx", "/lodi-memorial/" },
+            { "/napa-valley/Pages/ehealth/kramescontent/", "/st-helena/" },
+            { "/napa-valley/Pages/OHAM/", "/st-helena/" },
+            { "/nw/Pages/ehealth/kramescontent/", "/portland/" },
+            { "/nw/pages/news/newssearchresult.aspx", "/blog/" },
+            { "/nw/Pages/PNRS/", "/portland" },
+            { "/Pages/eHealth/AdamContent/", "/blog/" },
+            { "/Pages/eHealth/kramesContent/", "/blog/" },
+            { "/Pages/ENRS/", "/" },
+            { "/Pages/pnrs/", "/doctors" },
+            { "/portland/event/", "/portland/" },
+            { "/Portland/Pages/ehealth/kramescontent/", "/portland/" },
+            { "/Portland/Pages/OHAM/", "/portland/" },
+            { "/Portland/pages/pnrs/", "/portland/" },
+            { "/Simi-Valley/event/", "/simi-valley/" },
+            { "/Simi-Valley/pages/ehealth/", "/simi-valley/" },
+            { "/Simi-Valley/pages/enrs/", "/simi-valley/" },
+            { "/Simi-Valley/Pages/OHAM/", "/simi-valley/" },
+            { "/sonora/Pages/ehealth/kramescontent/", "/sonora/" },
+            { "/sonora-regional/Pages/ehealth/kramescontent/", "/sonora/" },
+            { "/sthelena/Pages/OHAM/", "/st-helena/" },
+            { "/TehachapiValley/Pages/eHealth/KramesContent/", "/tehachapi-valley/" },
+            { "/TehachapiValley/Pages/OHAM/", "/tehachapi-valley/" },
+            { "/Tillamook/Pages/ehealth/kramescontent/", "/tillamook/" },
+            { "/Tillamook/pages/enrs/", "/tillamook/" },
+            { "/Tillamook/Pages/OHAM/", "/tillamook/" },
+            { "/Tillamook/pages/pnrs/", "/doctors/" },
+            { "/trmc/Pages/ehealth/kramescontent/", "/tillamook/" },
+            { "/trmc/pages/pnrs/", "/doctors/" },
+            { "/ukiah-valley/pages/clinicaltrials/", "/ukiah-valley/" },
+            { "/ukiah-valley/Pages/OHAM/", "/locations/" },
+            { "/vallejo/Pages/ehealth/kramescontent/", "/vallejo/" },
+            { "/walla-walla/", "/find-a-location/" },
+            { "/white-memorial/event/", "/white-memorial/" },
+            { "/white-memorial/Pages/ehealth/kramescontent/", "/white-memorial/" },
+            { "/white-memorial/pages/enrs/", "/white-memorial/" },
+            { "/white-memorial/Pages/OHAM/", "/white-memorial/" }
+        };
+        public static string[,] subProjects =
+        {
+            { "/white-memorial/", "/white-memorial/" },
+            { "/tehachapivalley/", "/tehachapivalley/" },
+            { "/bakersfield/", "/bakersfield/" }
+
+        };
         public static string[] osDoctorTitles = { "-md", "-do", "-mph", "-pa-c", "-macp", "-mba", "-agacnp-bc", "-np", "-np-c", "-fnp-c", "-msn", "-aprn", "-phd", "-ms", "-cnm", "-facs", "-facog", "-facp", "-dpm", "-mmci", "-rd", "-cdn", "-msc", "-facmg", "-fapa", "-mhs", "-faaa", "-np-bc", "-cgc", "-facr", "-med", "-whnp-bc" };
 
         public static List<string> nsBlogList = new List<string>();
@@ -40,20 +105,17 @@ namespace RedirectMachine
         {
 
             // initialize paths to files
-            //string osUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\OldSiteUrls.csv";
             string osUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\OldSiteUrls.csv";
+            //string osUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\TestBatch.csv";
             string nsUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\NewSiteUrls.csv";
-            string lostUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\LostUrls.csv";
-            string foundUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\FoundUrls.csv";
-
-            string osDocUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\OldSite_docs.csv";
-            string osBlogUrlFile = @"C:\Users\timothy.darrow\source\repos\RedirectMachine\OldSite_blog.csv";
-
+            string lostUrlFile = @"C:\Users\timothy.darrow\Downloads\LostUrls.csv";
+            string foundUrlFile = @"C:\Users\timothy.darrow\Downloads\FoundUrls.csv";
 
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            ReadCSV(osUrls, osUrlFile);
+            ReadCSV(osUrls, osUrlFile, osParams);
+            //ReadCSV(osUrls, osUrlFile);
             ReadCSV(nsUrls, nsUrlFile);
 
             //LogList(osUrls);
@@ -71,14 +133,8 @@ namespace RedirectMachine
             // search url lists for new items
             //findUrl(osBlogList, nsBlogList);
             //findDocUrl(osDoctorList, nsDoctorList, osDoctorTitles);
-            //FilterUrls(osUrls, osParams);
+            FilterUrls(osUrls, osParams);
             findUrl(osUrls, nsUrls);
-
-            foreach (var item in osParams)
-            {
-                Console.WriteLine($"item[0]: {item[0]}");
-                Console.WriteLine($"item[1]: {item[1]}");
-            }
 
             buildCSV(lostList, lostUrlFile);
             buildCSV(foundList, foundUrlFile);
@@ -99,13 +155,16 @@ namespace RedirectMachine
 
         // End of main function
 
-        public static void FilterUrls(List<string> list, string[,] i)
+        public static void FilterUrls(List<string> list, string[,] keyVals)
         {
-            foreach (var j in i)
+            // purpose of function: the reps multidimensional array houses a key/value pair
+            // The key is a url that occurs repeatedly in the old site map.
+            // The value is the new site's url that will be where all urls using the key will redirect to
+
+            for (int i = 0; i < keyVals.GetLength(0); i++)
             {
-                list.RemoveAll(item => item == j[0].ToString());
-                list.Add(j[1].ToString());
-                Console.WriteLine($"line 102 - j[1]: {j[1].ToString()}");
+                list.RemoveAll(item => item == keyVals[i, 0].ToString().ToLower());
+                foundList.Add($"{keyVals[i, 0].ToString()}*,{keyVals[i, 1].ToString()}");
             }
         }
 
@@ -115,6 +174,10 @@ namespace RedirectMachine
             {
                 if (!checkList(path, newList))
                 {
+                    if (path == "")
+                    {
+
+                    }
                     lostMatch++;
                     lostList.Add(path);
                 }
@@ -172,9 +235,7 @@ namespace RedirectMachine
             else if (temp.EndsWith("/*"))
                 index = getIndex(temp, "/*");
             else if (temp.Contains(".aspx"))
-            {
                 index = getIndex(temp, ".aspx");
-            }
             temp = temp.Substring(0, index);
 
             int pos = temp.LastIndexOf("/") + 1;
@@ -203,25 +264,75 @@ namespace RedirectMachine
             }
         }
 
+        static void ReadCSV(List<string> list, string filePath, string[,] keyVals)
+        {
+            int counter = 0;
+            // add CSV file contents to list
+            
+            using (var reader = new StreamReader(@"" + filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    bool catchAll = false;
+                    var line = reader.ReadLine();
+                    var temp = line.ToLower().Trim('"');
+                    for (int i = 0; i < keyVals.GetLength(0); i++)
+                    {
+                        if (temp.StartsWith(keyVals[i, 0].ToString().ToLower()))
+                        {
+                            catchAll = true;
+                            counter++;
+                            break;
+                        }
+                    }
+                    if (catchAll == false)
+                        list.Add(line);
+                }
+                list.Sort();
+                Console.WriteLine($"Counter: {counter}");
+            }
+        }
+
         static void PrintList(List<string> list)
         {
+            // method for testing purposes only
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
         }
 
-        // method that builds a new CSV for the user to view at the specified file path
+        
         static void buildCSV(List<string> list, string filePath)
         {
+            // method that builds a new CSV for the user to view at the specified file path
             using (TextWriter tw = new StreamWriter(@"" + filePath))
             {
                 foreach (var item in list)
                 {
-                    var newLine = string.Format("{0}", item);
-                    tw.WriteLine(newLine);
+                    tw.WriteLine(item);
                 }
             }
         }
     }
+
+    /*
+    public class PriorityObject
+    {
+        private string url { get; set; }
+        public int priority { get; set; }
+
+        public PriorityObject()
+        {
+            url = "";
+            priority = 0;
+        }
+
+        public PriorityObject(string url)
+        {
+            this.url = url;
+        }
+
+    }
+    */
 }
