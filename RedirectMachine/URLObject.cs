2133e003c9b8d7e1eca76f71188387e9e21d42a8
Tim Darrow <timothy.honor7@gmail.com>
2019-01-08 17:39:55 -0800

    New Object Creation
    Created UrlObject class

---
+++ RedirectMachine/URLObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedirectMachine
+{
+    class URLObject
+    {
+        private string originalUrl;
+        private string urlSubDirectory;
+        private int score = 0;
+        public string[] matchedUrls = new string[]{};
+
+        URLObject urlObject = new URLObject();
+    }
+}
