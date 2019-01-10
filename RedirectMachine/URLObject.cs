b218c04fb11872dcb054324a6bee41d866d21a46
Tim Darrow <timothy.honor7@gmail.com>
2019-01-09 20:29:20 -0800

    Updates to TruncateString methods
    Added TruncateString method to new URLObject Class with all necessary components. Created new class to check for unnecessary components at end of url

--- RedirectMachine/URLObject.cs
+++ RedirectMachine/URLObject.cs
@@ -86,11 +86,88 @@ namespace RedirectMachine
             {
                 foreach (var url in matchedUrls)
                 {
-                    
+                    string temp = TruncateString(url);
                 }
             }
 
                 return false;
+        }
+
+        public string TruncateString(string value)
+        {
+            // Purpose of method: retrieve usable/searchable end of url from variable value.
+            // Get url text after last slash in url
+            string temp = CheckVars(value);
+            int index = value.Length;
+            int pos = temp.LastIndexOf("/") + 1;
+            temp = temp.Substring(pos, temp.Length - pos);
+            return temp;
+        }
+
+        public string TruncateString(string value, int maxLength)
+        {
+            // Purpose of method: retrieve usable/searchable end of url from variable value.
+            
+            // Get url text after last slash in url,
+            // truncate temporary value to maxLength
+            string temp = CheckVars(value);
+            int index = value.Length;
+            int pos = temp.LastIndexOf("/") + 1;
+            temp = temp.Substring(pos, temp.Length - pos);
+            if (string.IsNullOrEmpty(temp)) return temp;
+            return temp.Length <= maxLength ? temp : temp.Substring(0, maxLength);
+        }
+
+        public string CheckVars(string value)
+        {
+            // Purpose: remove unnecessary contents on end of url if found
+            if (value.EndsWith("/"))
+                value = GetSubString(value, "/", false);
+            else if (value.EndsWith("/*"))
+                value = GetSubString(value, "/*", false);
+            else if (value.EndsWith("-"))
+                value = GetSubString(value, "-", false);
+            else if (value.Contains("."))
+                value = GetSubString(value, ".", false);
+            return value;
+        }
+
+        public static int GetIndex(string i, string j)
+        {
+            // Purpose of method: return position of j variable in string i.
+            return i.LastIndexOf(j);
+        }
+
+        public static string GetSubString(string i, string j, bool x)
+        {
+            // Purpose of method: return the substring of the string that is passed into this function.
+            // This method is overloaded with a bool. The bool indicates to the function that it must return a substring
+            // 1) if true, includes the string j rather than excluding it, or
+            // 2) if false, returns a substring that excludes string j.
+            int index = GetIndex(i, j);
+            string temp;
+            if (x == true)
+            {
+                temp = i.Substring(0, index + j.Length);
+            }
+            else
+                temp = i.Substring(0, index);
+            return temp;
+        }
+
+        public static string GetSubString(string i, string j, int x)
+        {
+            // Purpose of method: return the substring of the string that is passed into this function.
+            // This method is overloaded with an int. The int indicates to the function that it must rerun that many times.
+            var pos = 0;
+            string temp = i;
+            while (pos <= x)
+            {
+                int index = GetIndex(i, j);
+                temp = temp.Substring(0, index);
+                pos++;
+            }
+            return temp;
         }
     }
 
